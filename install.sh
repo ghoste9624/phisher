eee510bc7557fbeebfb133a7c93173deb7ee83f4
ghoste9624 <jackspade44@outlook.com>
2025-06-08 02:42:07 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/princekrvert/mask.git 
+cd mask
+npm install 
+npm install axios
+npm install prompt-sync 
+npm audit fix --force 
+clear ; cd .. ; mv mask.on.sh mask ; echo "" ; echo "" ; ls -a
+echo ""
+echo -e "\e[97m>>> \e[1;92mInstallation Successful...\e[0m"
